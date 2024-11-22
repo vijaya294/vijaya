861d80d7d891d7224f0ad3a23351db2a19c59a7c
Srilatha <koduri0311@gmail.com>
2024-11-22 14:34:03 +0000

    date and time

--- 01.date-time.sh
+++ 01.date-time.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-TIMESTAMP = $(date +%d-%m-%Y-%S-%M-%H)
+
 
 #TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 #Script execution date and time is: Fri Nov 22 14:30:24 UTC 2024
 
-#echo "Script started executing at: $(date)" 
+
 
 echo "Script execution date and time is: $(date)"
 
