18fda1e85868705e8968b03bf3e75f56fb26dcbf
Srilatha <koduri0311@gmail.com>
2024-11-23 16:48:32 +0000

    date and time

--- 02.if-function.sh
+++ 02.if-function.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-NUMBER = $1
+NUMBER=$1
 
 if [ $NUMBER -gt 20 ]
 then 
     echo "given number $NUMBER is greater than 20"
 else
     echo "given number $NUMBER is less than 20"
-
 fi
 
