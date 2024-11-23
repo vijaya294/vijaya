d14fca233feac238e04709d46008c495082dfde3
Srilatha <koduri0311@gmail.com>
2024-11-23 17:29:55 +0000

    whether hard or soft link

---
+++ 06.installing-package.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
+}
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+else
+    echo "Git is already installed, nothing to do.."
+fi
+
